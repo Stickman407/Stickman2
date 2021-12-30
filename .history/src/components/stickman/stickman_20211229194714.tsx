657e307b130943fe171320356cdf3e8c654b24f2
Bobby Johnson <bobby@madnesslabs.net>
2021-12-29 19:52:03 -0600

    fix(): Correct StencilJS Setup

---
+++ .history/src/components/stickman/stickman_20211229194714.tsx
@@ -0,0 +1,24 @@
+import { Component, h } from '@stencil/core';
+
+
+@Component({
+    tag: 'stickman',
+    styleUrl: 'stickman.css'
+})
+export class Stickman {
+
+    render() {
+        return (
+            <div class="stickman">
+                <div class="leg2"></div>
+                <div class="leg"></div>
+                <div class="torso"></div>
+                <div class="arm"></div>
+                <div class="arm2"></div>
+                <div class="head">
+                    <div class="headhole"></div>
+                </div>
+            </div>
+        );
+    }
+}
